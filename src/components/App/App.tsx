83d3fe8299b84221124a05ef300f81738a62d4a2
Anastasiia <gorgona1597531597@gmail.com>
2025-08-31 19:14:30 +0200

    add search form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,41 @@
-import OrderForm from "../OrderForm/OrderForm";
+// import OrderForm from "../OrderForm/OrderForm";
+
+import { useState } from "react";
+import SearchForm from "../ SearchForm/SearchForm";
+import axios from "axios";
+import type { Article } from "../../types/article";
+import ArticleList from "../ArticleList/ArticleList";
+
+// function App() {
+//     const handleOrder = (data: string) => {
+//         console.log("Order:", data);
+        
+//     }
+
+//     return <div>
+//     <OrderForm onSubmit={handleOrder} />
+// </div>
+// }
+
+interface ArticlesHttpResponse {
+  hits: Article[];
+}
 
 function App() {
-    const handleOrder = (data: string) => {
-        console.log("Order:", data);
+    const [articles, setArticles] = useState<Article[]>([]);
+
+    const handleSearch = async (topic: string) => {
+        const response = await axios.get<ArticlesHttpResponse>
+            (`https://hn.algolia.com/api/v1/search?query=${topic}`);
+        
+        setArticles(response.data.hits);
         
     }
 
     return <div>
-        <h1>Place your order</h1>
-    <OrderForm onSubmit={handleOrder} />
-</div>
+        <SearchForm onSubmit={handleSearch} />
+        {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
 }
 
 export default App
